706e39c18992293803edccdf2af0112a1147cd8a
calebgregory <calebgregory@gmail.com>
2015-05-13 13:54:40 -0500

    Copy other files during build

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 # clean and prepare public directory
 rm -rf public
-mkdir public
+cp -r src public
+
 
 # compile jade to html ( -P : prettify )
 ./node_modules/.bin/jade src -P
@@ -21,5 +22,11 @@ rm -rf src/**/*.html \
   src/_styles/main.scss public/css/main.css
 
 # convert ES6 JS to ES5
-
 ./node_modules/.bin/babel src --out-dir public -s inline
+
+# clean unneeded files
+rm -rf public/_styles \
+       public/*.jade \
+       public/**/*.jade \
+       public/*.scss \
+       public/**/*.scss
